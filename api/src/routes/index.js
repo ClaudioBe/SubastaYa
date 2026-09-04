34f8ae8480993adb93d7fec9859b6da0a92c4cd3
MateoMontiel <montielmateo23@gmail.com>
2026-09-04 19:58:02 -0300

    Routa y controlador de Billetera

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,8 @@
 const {Router}= require("express")
 const {subastaRouter}=require('./subastaRouter')
+const {billeteraRouter}=require('./billeteraRouter')
 const router=Router();
 router.use("/subastas",subastaRouter)
-
+router.use("/billetera",billeteraRouter)
 
 module.exports=router;
